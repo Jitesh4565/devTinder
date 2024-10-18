2e55e6661aa2fa228f8c96d1bfed1a068ab367b0
Jitesh4565 <jitujadhav28@gmail.com>
2024-10-18 16:41:21 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,18 +2,26 @@ const express=require('express');
 
 const app=express();
 
-app.use("/hello", (req,res)=>{
-    res.send("Hello how you doin!");
-});
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Jitesh",lastName:"Jadhav"});
+})
+app.post("/user",(req,res)=>{
+    res.send("Data saved succesfully!")
+})
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted from the database!!");
+})
+//app.use("/hello", (req,res)=>{
+//     res.send("Hello how you doin!");
+// });
 
-app.use("/test",(req,res)=>{
-    res.send("Welcome to Namaste NodeJs!!");
-});
+// app.use("/test",(req,res)=>{
+//     res.send("Welcome to Namaste NodeJs!!");
+// });
 
-app.use("/",(req,res)=>{
-    res.send("Hello from the homepage");
-});
-
+// app.use("/",(req,res)=>{
+//     res.send("Hello from the homepage");
+// });
 app.listen(7777, ()=>{
     console.log("Server is listening");
 });
